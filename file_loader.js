5d6444f8c49b5c740560fd6f7e098200c542fcd5
Manassés Souza <manasses@ciandt.com>
2016-11-15 18:19:59 -0200

    invoke spreadsheet.js

--- file_loader.js
+++ file_loader.js
@@ -1,5 +1,8 @@
-var s = require("./bower_components/underscore.string");
-
+var fs = require('fs');
+var s = require("./node_modules/underscore.string");
+var updateSpreadsheet = require('./spreadsheet');
+// var HashMap = require('hashmap');
+var Map = require("collections/map");
 /**
  * Lists the files which name contains 'Comprovante' and were modified today
  *
@@ -27,16 +30,38 @@ var list = function listSpecificModified(auth, google) {
     if (files.length == 0) {
       console.log('No files found.');
     } else {
+      bills_map = new Map();
       console.log('Files:');
       for (var i = 0; i < files.length; i++) {
         var file = files[i];
         console.log('%s (%s)', file.title, file.id);
-        billing_value = s.strRightBack(file.title, "_");
+        billing_value = s.strRightBack(file.title, "_")
+        billing_value = s.strLeftBack(billing_value, ".");
+        // TODO: extract value from file name
+        console.log(billing_value)
+        first_limiter_occur = file.title.indexOf("_") + 1;
+        last_limiter_occur = file.title.lastIndexOf("_");
+        receipt_name = file.title.substring(first_limiter_occur, last_limiter_occur);
+        console.log(receipt_name);
+        bills_map.set(receipt_name, billing_value);
       }
+      console.log(bills_map)
+      fs.readFile('bills_data_map.json', function process(err, content) {
+        var body = JSON.parse(content);
+        console.log(body);
+        // content_map = new Map(strb);
+        // console.log('content: ' + content_map);
+        spreadsheet_map = new Map();
+        bills_map.forEach(function(value, key) {
+          console.log(key + " : " + value);
+          console.log(body[key]);
+          bill_key = body[key];
+          spreadsheet_map.set(bill_key, value);
+        });
+        console.log('spreadsheets: ' + spreadsheet_map);
+        // updateSpreadsheet(content);
+      });
     }
-    // TODO: extract value from file name
-    console.log(billing_value)
-    // TODO: extract name of receipt
   });
 }
 
--- spreadsheet.js
+++ spreadsheet.js
@@ -4,7 +4,9 @@ var m = require('moment')
 // var async = require('async');
 
 // service account created credentials
-var credentials = require('./SmartFinance-Bills-Beta-bb915af4e186.json');
+var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
+    process.env.USERPROFILE) + '/.credentials/'
+var credentials = require(TOKEN_DIR + 'SmartFinance-Bills-Beta-bb915af4e186.json');
 // spreadsheet key is the long id in the sheets URL
 // TODO: receive spreadsheet id as an argument (come from HTTP request for example)
 var my_sheet = new GoogleSpreadsheet('1zqc0BDV3l5wq7tEzJZF2GhFZUc4e213gaYcT3Zb3OyQ')
@@ -12,7 +14,8 @@ var bills_sheet;
 
 // TODO: extract the column with all the group cost categories
 // TODO: create a comparison table for: "extracted file name x group cost name"
-// function updateSpreadsheet() {
+var sheet = function updateSpreadsheet(data_map) {
+	console.log("data map: " + data_map);
 	my_sheet.useServiceAccountAuth(credentials, (err, token) => {
 		my_sheet.getInfo(function(err, info) {
 	    console.log('Loaded doc: '+info.title+' by '+info.author.email);
@@ -22,7 +25,7 @@ var bills_sheet;
 			workingCells();
 	  });
 	})
-// }
+}
 
 // function workingRow(rowNum) {
 // 	bills_sheet.getRows({
@@ -39,21 +42,21 @@ var bills_sheet;
 // 	});
 // }
 
-function monthReference(monthRowNum) {
+function monthReference(month_row_num) {
 	// TODO: externalize locale
 	m.locale('pt-BR')
 	bills_sheet.getCells({
-		'min-row': monthRowNum,
-		'max-row': monthRowNum,
+		'min-row': month_row_num,
+		'max-row': month_row_num,
 		'return-empty': false
 	}, function (err, cells) {
 		// TODO: externalize date format
 		var currentMonth = m().format('MMMM/YYYY')
 		console.log(currentMonth)
 		var lastUpdatedMonth = m(cells[cells.length -1])
-		// console.log(m(currentMonth).isAfter(lastUpdatedMonth));
+		console.log(lastUpdatedMonth);
 		console.log(m().isAfter(lastUpdatedMonth));
-		if (currentMonth !== lastUpdatedMonth) {
+		if (currentMonth !== lastUpdatedMonth && m().isAfter(lastUpdatedMonth)) {
 			// TODO: Add 2 columns and merge cell with month information
 		}
 		// TODO: update cell
@@ -75,3 +78,5 @@ function workingCells(cb) {
 		});
 	});
 }
+
+module.exports = sheet;
