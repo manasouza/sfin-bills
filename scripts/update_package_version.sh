1d879bc6fc1727bfea31c6d254befbffd4b83767
Manassés Souza <manasses.inatel@gmail.com>
2019-04-06 10:57:43 -0300

    attempt to run update_package_version (wip)

--- scripts/update_package_version.sh
+++ scripts/update_package_version.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
+# stepup notes add --section ()...)
+# TAG=$(stepup version --next-release | sed 's/\.[^.]*$//')
+
+# $ node -e "console.log(JSON.stringify(JSON.parse(process.argv[1]), null, '\t'));" '{"foo":"lorem","bar":"ipsum"}'
 TAG=$(git tag -l --sort=-v:refname | head -1 | sed 's/\.[^.]*$//')
-cat package.json | jq --arg newversion $TAG '.version=$newversion' > package.json
+echo $(cat package.json | jq --arg newversion $TAG '.version=$newversion') <<< package.json
+echo $(cat package.json | jq --arg newversion $TAG '.version=$newversion') | sed -e 's/^/"/g' -e 's/$/"/g'
+# stepup version create
