24c5ae4089abcaa632002fe0fb55b146f3f26061
Manassés Souza <manasses.inatel@gmail.com>
2019-04-08 21:14:13 -0300

    [#51] update to use async.waterfall

--- spreadsheet.js
+++ spreadsheet.js
@@ -17,6 +17,8 @@ var bills_sheet;
 var working_col;
 var currency_factor = 100;
 
+const CATEGORY_COLUMN = process.env.category_column
+
 var self = module.exports = {
 
   updateSpreadsheet : function(data_map, saveProcessedFiles) {
@@ -24,24 +26,121 @@ var self = module.exports = {
   		my_sheet.getInfo(function(err, info) {
         bills_sheet = info.worksheets[0];
         console.log('[INFO] Loaded doc: %s on first sheet: %s', info.title, bills_sheet.title);
-        async.series([
-          function (callback) {
-            console.log('[DEBUG] getting month reference');
-            // TODO: 2 is the months row
-            monthReference(2, validateFoundMonthReference);
-            callback(null, 'monthReference');
-          },
-          function (callback) {
-            console.log('[DEBUG] find category cells and set values');
-            // TODO: 1 is the categories column
-            workingRows(1, data_map, setCellValueForEachCategory);
-            callback(null, 'workingRows');
-          }
+        async.waterfall([
+          async.apply(monthReference, 2),
+          validateFoundMonthReference,
+          workingRows,
+          setCellValueForEachCategory
         ],
         function (err, result) {
-          console.log(result);
+          if (err) {
+            console.log('[ERROR] %s', err);
+          }
           saveProcessedFiles();
         });
+
+        function monthReference(month_row_num, callback) {
+          console.log('[DEBUG] getting month reference');
+          // TODO: externalize locale
+          m.locale('pt-BR');
+          bills_sheet.getCells({
+            'min-row': month_row_num,
+            'max-row': month_row_num,
+            'return-empty': false
+          }, function (err, cells) {
+            if (err) {
+              console.log('[ERROR] %s', err);
+            }
+            // TODO: externalize date format
+            var current_month = m().format('MMMM/YYYY');
+            var cells_processed = 0;
+            console.log('[INFO] current_month: %s', current_month);
+            cells.forEach(function (cell) {
+              if (current_month.toLowerCase() === cell.value.toLowerCase()) {
+                working_col = cell.col;
+              }
+              cells_processed++;
+              if (cells_processed === cells.length) {
+                console.log('working_col: ' + working_col);
+                callback(null);
+              }
+            });
+          });
+        }
+
+        function validateFoundMonthReference(callback) {
+          if (!working_col) {
+            console.log("[ERROR] month reference cell not found, so working_col not set");
+            process.exit(1);
+          }
+          if (!CATEGORY_COLUMN) {
+            console.log("[ERROR] Default category column not set");
+            process.exit(1);
+          }
+          callback(null, CATEGORY_COLUMN, data_map);
+        }
+
+        function workingRows(category_col, data_map, callback) {
+          console.log('[DEBUG] find category cells and set values');
+          bills_sheet.getCells({
+            'min-col': category_col,
+            'max-col': category_col,
+            'return-empty': false
+          }, function (err, cells) {
+            if (err) {
+              console.log("[ERROR] %s", err);
+            }
+            var category_rows = [];
+            var category_value_map = new Map();
+            cells.forEach(function (cell) {
+              if (data_map.has(cell.value)) {
+                category_rows.push(cell.row);
+                category_value_map.set(cell.row, cell.value);
+              }
+            });
+            callback(null, data_map, category_rows, category_value_map);
+          });
+        }
+
+        function setCellValueForEachCategory(data_map, category_rows, category_value_map, callback) {
+          if (category_value_map.length < 1) {
+            console.warn("[WARN] category values not mapped. Stucked here");
+          }
+          category_value_map.forEach(function (billingName, sheetRow) {
+            console.log("[DEBUG] key: %s / working_col: %s", billingName, working_col);
+            bills_sheet.getCells({
+              'min-row': sheetRow,
+              'max-row': sheetRow,
+              'min-col': working_col,
+              'max-col': working_col,
+              'return-empty': true
+            }, function (err, cells) {
+              if (err != null) {
+                console.log("[ERROR] %s", err);
+                return;
+              }
+              cells.forEach(function (cell) {
+                console.log('[DEBUG] cell row: %s / value: %s', cell.row, billingName);
+                data_map.get(billingName).forEach(function (value, listKey, collection) {
+                  value = self.convertToCurrency(value);
+                  if (_.isEmpty(cell._value)) {
+                    cell.formula = `=${value}`;
+                  } else {
+                    cell.formula = `${cell.formula}+${value}`;
+                  }
+                });
+                cell.save(function (err) {
+                  if (err) {
+                    console.log("[ERROR] error updating spreadsheet: %s", err);
+                  } else {
+                    console.log("[INFO] spreadsheet cell updated");
+                  }
+                });
+              });
+            });
+          });
+          callback(null);
+        }
   	  });
   	});
   },
@@ -72,100 +171,6 @@ var self = module.exports = {
     }
     callback(current_values);
   }
-
-  function monthReference(month_row_num, cb) {
-  	// TODO: externalize locale
-  	m.locale('pt-BR');
-  	bills_sheet.getCells({
-  		'min-row': month_row_num,
-  		'max-row': month_row_num,
-  		'return-empty': false
-  	}, function (err, cells) {
-      if (err) {
-        console.log('[ERROR] %s', err);
-      }
-  		// TODO: externalize date format
-  		var current_month = m().format('MMMM/YYYY');
-      var cells_processed = 0;
-  		console.log('[INFO] current_month: %s', current_month);
-      cells.forEach(function (cell) {
-        if (current_month.toLowerCase() === cell.value.toLowerCase()) {
-          working_col = cell.col;
-        }
-        cells_processed++;
-        if (cells_processed === cells.length) {
-          cb();
-        }
-      });
-  	});
-  }
-  
-  function validateFoundMonthReference() {
-    if (!working_col) {
-      console.log("[ERROR] month reference cell not found, so working_col not set");
-      process.exit(1);
-    }
-  }
-
-  function workingRows(category_col, data_map, callback) {
-    bills_sheet.getCells({
-      'min-col': category_col,
-      'max-col': category_col,
-      'return-empty': false
-    }, function (err, cells) {
-        if (err) {
-          console.log("[ERROR] %s", err);
-        }
-        var category_rows = [];
-        var category_value_map = new Map();
-        cells.forEach(function (cell) {
-          if (data_map.has(cell.value)) {
-            category_rows.push(cell.row);
-            category_value_map.set(cell.row, cell.value);
-          }
-        });
-        callback(data_map, category_rows, category_value_map);
-    });
-  }
-
-function setCellValueForEachCategory(data_map, category_rows, category_value_map) {
-  if (category_value_map.length < 1) {
-    console.warn("[WARN] category values not mapped. Stucked here");
-  }
-  category_value_map.forEach(function (billingName, sheetRow) {
-    console.log("[DEBUG] key: %s / working_col: %s", billingName, working_col);
-    bills_sheet.getCells({
-      'min-row': sheetRow,
-      'max-row': sheetRow,
-      'min-col': working_col,
-      'max-col': working_col,
-      'return-empty': true
-    }, function (err, cells) {
-      if (err != null) {
-        console.log("[ERROR] %s", err);
-        return;
-      }
-      cells.forEach(function (cell) {
-        console.log('[DEBUG] cell row: %s / value: %s', cell.row, billingName);
-        data_map.get(billingName).forEach(function (value, listKey, collection) {
-          value = self.convertToCurrency(value);
-          if (_.isEmpty(cell._value)) {
-            cell.formula = `=${value}`;
-          } else {
-            cell.formula = `${cell.formula}+${value}`;
-          }
-        });
-        cell.save(function (err) {
-          if (err) {
-            console.log("[ERROR] error updating spreadsheet: %s", err);
-          } else {
-            console.log("[INFO] spreadsheet cell updated");
-          }
-        });
-      });
-    });
-  });
-}
 
   function createNewColumns(month, row, col) {
     console.log('monthReference callback: ' + month +', '+ row +', '+ col);
