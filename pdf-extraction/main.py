5cbb933cfdde091b5a8285553efb0c08ffd3206f
Manassés Loiola de Souza <manasses.inatel@gmail.com>
2020-06-21 18:34:36 -0300

    add hard coded implementation to insert sheet data

--- pdf-extraction/main.py
+++ pdf-extraction/main.py
@@ -1,12 +1,13 @@
-import base64
-import gspread
 import json
+import locale
 import re
-from google.cloud import vision
+from datetime import datetime
+
+import gspread
 from google.cloud import storage
-from google.protobuf import json_format
+from google.cloud import vision
 from google.oauth2.service_account import Credentials
-import os
+from google.protobuf import json_format
 
 SPREADSHEET_ID = "1zqc0BDV3l5wq7tEzJZF2GhFZUc4e213gaYcT3Zb3OyQ"
 SPREADSHEET_TAB = 'Projeção'
@@ -17,31 +18,75 @@ SHEETS_API_SCOPE = ['https://spreadsheets.google.com/feeds',
 storage_client = storage.Client()
 vision_client = vision.ImageAnnotatorClient()
 
+locale.setlocale(locale.LC_TIME, "pt_BR.utf8")
 
-# def extract_from_pdf(event, context):
+
+def _get_for_agua(pdf_file=None):
+    return pdf_file.startswith('RFATURA_DIR_FR2FAT16')
+
+
+def _get_for_luz(pdf_file=None):
+    return re.search('boleto_[\d\s]+', pdf_file)
+
+
+# bills = {'Água': _get_for_agua(), 'Luz': _get_for_luz()}
+bills = ['Água', 'Luz']
+
+
 def main_entry(request):
     # data = base64.b64decode(event['data']).decode('utf-8')
     # payload = json.loads(data)
     # gcs_source_uri=payload['bucket_source_file']
-    gcs_source_uri='gs://sfinbills/RFATURA_DIR_FR2FAT16__95_0000_235202004652902.PDF'
+    gcs_source_uri = 'gs://sfinbills/RFATURA_DIR_FR2FAT16__95_0000_235202004652902.PDF'
     # gcs_destination_uri=payload['bucket_destination']
-    gcs_destination_uri='gs://sfinbills/output'
+    gcs_destination_uri = 'gs://sfinbills/output'
+    category_column = 3
+
+    import ipdb;ipdb.set_trace()
+
+    # TODO: adapt from trigger from GCS
+    # extract file name
+    pdf_file = gcs_source_uri[gcs_source_uri.rfind('/')+1:]
+    # detect
+    for bill_category in bills:
+        if _get_for_agua(pdf_file):
+            detected_category = bill_category
+        elif _get_for_luz(pdf_file):
+            detected_category = bill_category
 
     # AUTH
     creds_key_json = get_auth_key()
 
     # VISION API
     # Supported mime_types are: 'application/pdf' and 'image/tiff'
-    send_bill_file_and_set_output(gcs_source_uri, gcs_destination_uri)
+    # send_bill_file_and_set_output(gcs_source_uri, gcs_destination_uri)
 
     # Once the request has completed and the output has been
     # written to GCS, we can list all the output files.
-    bill_content = get_bill_content(gcs_destination_uri)
+    # bill_content = get_bill_content(gcs_destination_uri)
 
     # GSPREAD
     main_worksheet = get_spreadsheet(creds_key_json)
-    for cell in main_worksheet.range(INIT_ROW, STATUS_COLUMN, main_worksheet.row_count, HASH_FILE_NAMES_COLUMN):
-        print(cell)
+    # retrieve cell rows that needs update
+    for category_cell in main_worksheet.range(1, category_column, main_worksheet.row_count, category_column):
+        if category_cell.value:
+            print(category_cell.value)
+        if category_cell.value == detected_category:
+            update_row = category_cell.row
+    print('update row: '+str(update_row))
+    # retrieve column for current month
+    datem = datetime.today().strftime("%B/%Y")
+    print(datem)
+
+    for header_cell in main_worksheet.range(1, 1, 3, main_worksheet.col_count):
+        if header_cell.value:
+            print(header_cell.value)
+        if header_cell.value.lower() == datem:
+            update_column = header_cell.col
+    print('update column: '+str(update_column))
+
+    main_worksheet.update_cell(update_row, update_column, 'R$ 78,9')
+
 
 def get_spreadsheet(creds_key_json):
     credentials = Credentials.from_service_account_info(
@@ -51,6 +96,7 @@ def get_spreadsheet(creds_key_json):
     gc = gspread.authorize(credentials)
     main_worksheet = gc.open_by_key(SPREADSHEET_ID).worksheet(SPREADSHEET_TAB)
     return main_worksheet
+
 
 def get_bill_content(gcs_destination_uri):
     # Once the request has completed and the output has been
@@ -87,6 +133,7 @@ def get_bill_content(gcs_destination_uri):
         annotation.text))
     return annotation.text
 
+
 def send_bill_file_and_set_output(gcs_source_uri, gcs_destination_uri):
     # Supported mime_types are: 'application/pdf' and 'image/tiff'
     mime_type = 'application/pdf'
@@ -113,7 +160,6 @@ def send_bill_file_and_set_output(gcs_source_uri, gcs_destination_uri):
 
     print('Waiting for the operation to finish...')
     operation.result(timeout=420)
-    # TODO: generate current MM/YYYY
 
 
 def get_auth_key():
@@ -147,7 +193,7 @@ def get_auth_key():
     #             json_acct_info,
     #             scopes=SHEETS_API_SCOPE
     #         )
-    
+
     #         gc = gspread.authorize(credentials)
     #         main_worksheet = gc.open_by_key(SPREADSHEET_ID).worksheet(SPREADSHEET_TAB)
     #         print(main_worksheet)
